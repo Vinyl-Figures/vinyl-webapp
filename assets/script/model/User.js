01576f57790597c9919b5741f4a79f1528a24f1f
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-06-11 09:21:03 -0300

    doc(assets\script\model): add documentaion in user model class

--- assets/script/model/User.js
+++ assets/script/model/User.js
@@ -1,6 +1,19 @@
 
+/**
+ * Modelo que representa um usuário do sistema, 
+ * contendo informações como id, nome, email, senha e opções de acessibilidade.
+ */
 export class User {
     
+    /**
+     * Contrutor completo do modelo User, que recebe todas as informações do usuário, incluindo email e senha.
+     * 
+     * @param {number} id 
+     * @param {string} name 
+     * @param {string} email 
+     * @param {string} password 
+     * @param {list[string]} accessibilityOptions 
+     */
     constructor(id, name, email, password, accessibilityOptions) {
         this.id = id
         this.name = name
@@ -9,6 +22,13 @@ export class User {
         this.accessibilityOptions = accessibilityOptions
     }
 
+    /**
+     * Construtor simplificado do modelo User, que recebe apenas as informações essenciais do usuário, sem email e senha.
+     * 
+     * @param {number} id 
+     * @param {string} name 
+     * @param {list[string]} accessibilityOptions 
+     */
     constructor(id, name, accessibilityOptions) {
         this.id = id
         this.name = name
